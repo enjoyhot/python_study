7d7427e3488ed99ed822d25cde3fa69faa24bb85
enjoyhot <809778054@qq.com>
2016-08-08 21:44:29 +0800

    fix bug for mllib

--- reactJS_flask/project/app/main/views.py
+++ reactJS_flask/project/app/main/views.py
@@ -1,17 +1,25 @@
 # -*- coding:utf8 -*-
 from flask import render_template,json,request
 from . import main
-
+import os
 from pyspark import SparkContext,SparkConf
 print ("Successfully imported Spark Modules")
 
-from pyspark.mllib.classification import LogisticRegressionWithLBFGS, LogisticRegressionModel
+from pyspark.mllib.classification import LogisticRegressionWithLBFGS, LogisticRegressionModel, LabeledPoint
 print ("Successfully imported Mlib")
 
-sc = SparkContext("local[2]", "data analyse")
+# sc = SparkContext("local[2]", "data analyse")
 
 # conf = SparkConf().setAppName("data analyse").setMaster('spark://10.0.2.15:7077')  
 # sc = SparkContext(conf=conf)
+
+# config for SparkContext
+SPARK_HOME=os.environ['SPARK_HOME']
+conf = SparkConf().setMaster('spark://10.0.2.15:7077').set("spark.executor.memory", "2g").set("spark.cores.max","65");    
+site_packages = os.path.abspath((os.path.join(os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir), os.pardir))) + '/site-packages'
+pyspark = SPARK_HOME + "/python" + ":" + SPARK_HOME + "/python/lib/py4j-0.9-src.zip" + ":" + SPARK_HOME + "/python/lib/pyspark.zip"          
+conf.setExecutorEnv("PYTHONPATH", "$PYTHONPATH:" + site_packages + ":"  + pyspark + ":" + os.path.abspath(os.path.join(os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir), os.pardir)))
+
 
 ROOTDIR = '/home/gugugujiawei/projects/data'
 
@@ -29,10 +37,13 @@ def parsePoint(line):
     values = [float(x) for x in line.split(' ')]
     return LabeledPoint(values[0], values[1:])
 
-def logistic_model():
+def logistic_model(sc):
 
-    global sc
-    data = sc.textFile(ROOTDIR + "/sample_svm_data.txt")
+#    global conf
+#    conf.setAppName("data analyse")
+#    sc = SparkContext(conf=conf)
+#    print ("Successfully started SparkContext")  
+    data = sc.textFile("file://" + ROOTDIR + "/sample_svm_data.txt")
     parsedData = data.map(parsePoint)
     # Build the model
     model = LogisticRegressionWithLBFGS.train(parsedData)
@@ -44,15 +55,19 @@ def logistic_model():
 
 @main.route('/cls/logistic/post',methods=['POST'])
 def logistic_post():
-    global sc
     print request.form['userName']
     print request.form['userEmail']
+    global conf
+    conf.setAppName("data analyse")
+    sc = SparkContext(conf=conf)
+    print ("Successfully started SparkContext")  
 
-    data = sc.textFile(ROOTDIR + "/UserPurchaseHistory.csv").map(lambda line:
+    data = sc.textFile("file://" + ROOTDIR + "/UserPurchaseHistory.csv").map(lambda line:
     line.split(",")).map(lambda record: (record[0], record[1], record[2]))
     numPurchases = data.count()
     print numPurchases
 
-    # logistic_model()
+    logistic_model(sc)
+    sc.stop()
 
     return json.dumps({'msg': 'test','success': True})
--- reactJS_flask/run.sh
+++ reactJS_flask/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ROOT=$(dirname $0)
-export SPARK_HOME="/usr/local/src/spark-1.6.1-bin-hadoop2.6/"
-export PYTHONPATH="$ROOT/project/site-packages:$SPARK_HOME/python:$SPARK_HOME/python/lib/py4j-0.9-src.zip:$ROOT/python/lib/pyspark.zip"
+export SPARK_HOME="/usr/local/src/spark-1.6.1-bin-hadoop2.6"
+export PYTHONPATH="$ROOT/project/site-packages:$SPARK_HOME/python:$SPARK_HOME/python/lib/py4j-0.9-src.zip:$ROOT/python/lib/pyspark.zip:$PYTHONPATH"
 python ./project/manage.py runserver -t 0.0.0.0
